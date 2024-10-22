dfb249d8bd551a0e76904c4f61cd5ac22144fb82
HMLPHerath <156527514+HMLPHerath@users.noreply.github.com>
2024-10-23 00:17:58 +0530

    Student

--- src/main/java/com/mycompany/student_management_system/Student_Management_System.java
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -11,6 +11,6 @@ package com.mycompany.student_management_system;
 public class Student_Management_System {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Wlcome to java project");
     }
 }

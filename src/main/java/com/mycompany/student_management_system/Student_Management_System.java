7f71d7b96d58d166a8a0b468378b28e778fb2603
HMLPHerath <>
2024-10-23 00:00:53 +0530

    Student ස්‍ය්ස්ටෙම්

---
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student_management_system;
+
+/**
+ *
+ * @author User
+ */
+public class Student_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
